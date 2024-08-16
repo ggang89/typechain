182b5260cee76f5e01595d3ac0988a51514c0d9d
ggang89 <ggang89@gmail.com>
2024-08-16 22:38:38 +0900

    js와 ts함께 사용시=>@ts-check(js파일의 맨 윈에 추가하면 ts가 오류를 발생시킨다.) =>JSDoc를 작성해준다(js에 type정보를 제공할 수 있다. /** */)

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,23 @@
+
+//@ts-check
+//=>타입스크립트 파일한테 자바스크립트 파일을 확인하라고 알리는 것
+//JSDoc 코멘트:ts가 js파일을 확인해줌
+/**
+ * Initializes the project
+ * @param {object} config //object를 변수로 받는데, 매개변수 이름이 config
+ * @param {boolean} config.debug //config 객체안에는 boolean이 있다
+*@param {string} config.url 
+* @returns boolean
+ */
 export function init(config) {
   return true
 }
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
